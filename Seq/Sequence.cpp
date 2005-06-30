1f6ab75efff9ce896fb42593c3181d25422f28f2
khalid <khalid@80559d87-eeef-40de-8cae-50fbbb9f2701>
2005-06-30 13:47:23 +0000

    Change to setToSizeR

--- Seq/Sequence.cpp
+++ Seq/Sequence.cpp
@@ -5,7 +5,7 @@
 //
 
 /*
-Copyright ou © ou Copr. CNRS, (17 Novembre 2004) 
+Copyright ou © ou Copr. CNRS, (17 Novembre 2004)
 
 Julien.Dutheil@univ-montp2.fr
 
@@ -15,7 +15,7 @@ pour l'analyse de séquences.
 Ce logiciel est régi par la licence CeCILL soumise au droit français et
 respectant les principes de diffusion des logiciels libres. Vous pouvez
 utiliser, modifier et/ou redistribuer ce programme sous les conditions
-de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA 
+de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA
 sur le site "http://www.cecill.info".
 
 En contrepartie de l'accessibilité au code source et des droits de copie,
@@ -26,16 +26,16 @@ titulaire des droits patrimoniaux et les concédants successifs.
 
 A cet égard  l'attention de l'utilisateur est attirée sur les risques
 associés au chargement,  à l'utilisation,  à la modification et/ou au
-développement et à la reproduction du logiciel par l'utilisateur étant 
-donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+développement et à la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à
 manipuler et qui le réserve donc à des développeurs et des professionnels
 avertis possédant  des  connaissances  informatiques approfondies.  Les
 utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
 logiciel à leurs besoins dans des conditions permettant d'assurer la
-sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
-à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
 
-Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
 pris connaissance de la licence CeCILL, et que vous en avez accepté les
 termes.
 */
@@ -49,16 +49,16 @@ This software is a computer program whose purpose is to provide classes
 for sequences analysis.
 
 This software is governed by the CeCILL  license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
+abiding by the rules of distribution of free software.  You can  use,
 modify and/ or redistribute the software under the terms of the CeCILL
 license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info". 
+"http://www.cecill.info".
 
 As a counterpart to the access to the source code and  rights to copy,
 modify and redistribute granted by the license, users are provided only
 with a limited warranty  and the software's author,  the holder of the
 economic rights,  and the successive licensors  have only  limited
-liability. 
+liability.
 
 In this respect, the user's attention is drawn to the risks associated
 with loading,  using,  modifying and/or developing or reproducing the
@@ -67,9 +67,9 @@ that may mean  that it is complicated to manipulate,  and  that  also
 therefore means  that it is reserved for developers  and  experienced
 professionals having in-depth computer knowledge. Users are therefore
 encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
 
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
@@ -94,7 +94,7 @@ const bool Sequence::ANTISENSE = false;
 
 Sequence::Sequence(const string & name, const string & sequence, const Alphabet * alpha)
 throw (BadCharException) :
-	SymbolList(alpha), 
+	SymbolList(alpha),
 	_name(name),
 	_sense(true)
 {
@@ -125,7 +125,7 @@ throw (BadCharException) :
 	_comments(comments),
 	_sense(true)
 {}
-	
+
 Sequence::Sequence(const string & name, const vector<int> & sequence, const Alphabet * alpha)
 throw (BadIntException) :
 	SymbolList(sequence, alpha),
@@ -146,7 +146,7 @@ throw (BadIntException) :
 Sequence::Sequence(const Sequence & s) :
 	SymbolList(s),
 	_name(s.getName()),
-	_comments(s.getComments()), 
+	_comments(s.getComments()),
 	_sense(s.getSense()) { }
 
 /** Assignation operator: ***************************************************************/
@@ -207,7 +207,7 @@ void Sequence::setToSizeR(unsigned int size)
 	if (size == seqSize) return;
 
 	// Add gaps up to specified size
-	while(_content.size() < seqSize) _content.push_back(-1);	
+	while(_content.size() < size) _content.push_back(-1);
 }
 
 /****************************************************************************************/
