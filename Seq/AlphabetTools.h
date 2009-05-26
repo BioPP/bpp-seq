68af3aed97ad1614d84df9ce9bcbd9be174c13bc
jdutheil <jdutheil@80559d87-eeef-40de-8cae-50fbbb9f2701>
2009-05-26 07:47:26 +0000

    Restored commented function + formating changes.

--- Seq/AlphabetTools.h
+++ Seq/AlphabetTools.h
@@ -60,118 +60,126 @@ class AlphabetTools
     static const ProteicAlphabet PROTEIN_ALPHABET;
     static const DefaultAlphabet DEFAULT_ALPHABET;
 
-	public:
-		AlphabetTools() {}
-		virtual ~AlphabetTools() {}
-	
-	public:
-		/**
+  public:
+    AlphabetTools() {}
+    virtual ~AlphabetTools() {}
+  
+  public:
+    /**
      * @brief Character identification method for sequence's alphabet identification
      *
-		 * Return :
-		 * - -1 gap
+     * Return :
+     * - -1 gap
      * - 1 DNA specific (no character!)
      * - 2 RNA specific (U)
-		 * - 3 Protein specific (characters E, F, I, L, P, Q)
+     * - 3 Protein specific (characters E, F, I, L, P, Q)
      * - 4 Nucleotide specific (no character)
      * - 5 DNA or Protein specific (T)
      * - 6 RNA or Protein specific (no character)
-		 * - 7 Any alphabet (A, B, C, D, G, H, J, K, M, N, O, R, S, V, W, X, Y, Z, 0)
+     * - 7 Any alphabet (A, B, C, D, G, H, J, K, M, N, O, R, S, V, W, X, Y, Z, 0)
      * - 0 Unknown character
      *
      * @param state The character to test.
      * @return The type code.
-		 */
-		static int getType(char state);
+     */
+    static int getType(char state);
 
-		/**
-		 * @brief This checks that all characters in the alphabet are coded by a string of same length.
-		 *
-		 * This method is used when states are coded by more than one character, typically: codon alphabets.
-		 *
-		 * @param alphabet The alphabet to check.
-		 * @return True if all text description have the same length (e.g. 3 for codon alphabet).
-		 */
-		static bool checkAlphabetCodingSize(const Alphabet & alphabet) throw (AlphabetException);
+    /**
+     * @brief This checks that all characters in the alphabet are coded by a string of same length.
+     *
+     * This method is used when states are coded by more than one character, typically: codon alphabets.
+     *
+     * @param alphabet The alphabet to check.
+     * @return True if all text description have the same length (e.g. 3 for codon alphabet).
+     */
+    static bool checkAlphabetCodingSize(const Alphabet & alphabet) throw (AlphabetException);
 
-		/**
-		 * @brief This checks that all characters in the alphabet are coded by a string of same length.
-		 *
-		 * This function performs the same work as the previous one, but deals with pointers
-		 * instead of reference. This may be more convenient since we often use pointers on alphabets.
-		 *
-		 * @param alphabet a pointer toward the alphabet to check.
-		 * @return True if all text description have the same length (e.g. 3 for codon alphabet).
-		 */
-		static bool checkAlphabetCodingSize(const Alphabet * alphabet) throw (AlphabetException);
+    /**
+     * @brief This checks that all characters in the alphabet are coded by a string of same length.
+     *
+     * This function performs the same work as the previous one, but deals with pointers
+     * instead of reference. This may be more convenient since we often use pointers on alphabets.
+     *
+     * @param alphabet a pointer toward the alphabet to check.
+     * @return True if all text description have the same length (e.g. 3 for codon alphabet).
+     */
+    static bool checkAlphabetCodingSize(const Alphabet * alphabet) throw (AlphabetException);
 
-		/**
-		 * @brief In case that all states in the given alphabet have a string description of same length,
-		 * send this length; e.g. 3 for codon alphabets.
-		 *
-		 * @param alphabet The alphabet to analyse.
-		 * @return The common size of all text descriptionif there is one. Else throws an AlphabetException.
-		 */
-		static int getAlphabetCodingSize(const Alphabet & alphabet) throw (AlphabetException);
+    /**
+     * @brief In case that all states in the given alphabet have a string description of same length,
+     * send this length; e.g. 3 for codon alphabets.
+     *
+     * @param alphabet The alphabet to analyse.
+     * @return The common size of all text descriptionif there is one. Else throws an AlphabetException.
+     */
+    static int getAlphabetCodingSize(const Alphabet & alphabet) throw (AlphabetException);
 
-		/**
-		 * @brief In case that all states in the given alphabet have a string description of same length,
-		 * send this length; e.g. 3 for codon alphabets.
-		 *
-		 * This function performs the same work as the previous one, but deals with pointers
-		 * instead of reference. This may be more convenient since we often use pointers on alphabets.
-		 *
-		 * @param alphabet a pointer toward the alphabet to analyse.
-		 * @return The common size of all text descriptionif there is one. Else throws an AlphabetException.
-		 */
-  static int getAlphabetCodingSize(const Alphabet * alphabet) throw (AlphabetException);
-
-  /**
-   * @return True if the alphabet is an instanciation of the NucleicAlphabet class.
-   * @param alphabet The alphabet to check.
-   */
-  static bool isNucleicAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<NucleicAlphabet>(alphabet); }
-  /**
-   * @return True if the alphabet is an instanciation of the DNA class.
-   * @param alphabet The alphabet to check.
-   */
-  static bool isDNAAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<DNA>(alphabet); }
-  /**
-   * @return True if the alphabet is an instanciation of the RNA class.
-   * @param alphabet The alphabet to check.
-   */
-  static bool isRNAAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<RNA>(alphabet); }
-  /**
-   * @return True if the alphabet is an instanciation of the ProteicAlphabet class.
-   * @param alphabet The alphabet to check.
-   */
-  static bool isProteicAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<ProteicAlphabet>(alphabet); }
-  /**
-   * @return True if the alphabet is an instanciation of the Codon class.
-   * @param alphabet The alphabet to check.
-   */
-  static bool isCodonAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<CodonAlphabet>(alphabet); }
-  /**
-   * @return True if the alphabet is an instanciation of the WordAlphabet class.
-   * @param alphabet The alphabet to check.
-   */
-  static bool isWordAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<WordAlphabet>(alphabet); }
-  /**
-   * @return True if the alphabet is an instanciation of the DefaultAlphabet class.
-   * @param alphabet The alphabet to check.
-   */
-  static bool isDefaultAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<DefaultAlphabet>(alphabet); }
+    /**
+     * @brief In case that all states in the given alphabet have a string description of same length,
+     * send this length; e.g. 3 for codon alphabets.
+     *
+     * This function performs the same work as the previous one, but deals with pointers
+     * instead of reference. This may be more convenient since we often use pointers on alphabets.
+     *
+     * @param alphabet a pointer toward the alphabet to analyse.
+     * @return The common size of all text descriptionif there is one. Else throws an AlphabetException.
+     */
+    static int getAlphabetCodingSize(const Alphabet * alphabet) throw (AlphabetException);
   
-  /**
-   * @brief Tell if two characters match according to a given alphabet.
-   *
-     * Exemple (DNA):
+    /**
+     * @return True if the alphabet is an instanciation of the NucleicAlphabet class.
+     * @param alphabet The alphabet to check.
+     */
+    static bool isNucleicAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<NucleicAlphabet>(alphabet); }
+
+    /**
+     * @return True if the alphabet is an instanciation of the DNA class.
+     * @param alphabet The alphabet to check.
+     */
+    static bool isDNAAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<DNA>(alphabet); }
+    
+    /**
+     * @return True if the alphabet is an instanciation of the RNA class.
+     * @param alphabet The alphabet to check.
+     */
+    static bool isRNAAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<RNA>(alphabet); }
+  
+    /**
+     * @return True if the alphabet is an instanciation of the ProteicAlphabet class.
+     * @param alphabet The alphabet to check.
+     */
+    static bool isProteicAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<ProteicAlphabet>(alphabet); }
+  
+    /**
+     * @return True if the alphabet is an instanciation of the Codon class.
+     * @param alphabet The alphabet to check.
+     */
+    static bool isCodonAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<CodonAlphabet>(alphabet); }
+  
+    /**
+     * @return True if the alphabet is an instanciation of the WordAlphabet class.
+     * @param alphabet The alphabet to check.
+     */
+    static bool isWordAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<WordAlphabet>(alphabet); }
+    
+    /**
+     * @return True if the alphabet is an instanciation of the DefaultAlphabet class.
+     * @param alphabet The alphabet to check.
+     */
+    static bool isDefaultAlphabet(const Alphabet * alphabet) { return alphabetInheritsFrom<DefaultAlphabet>(alphabet); }
+  
+    /**
+     * @brief Tell if two characters match according to a given alphabet.
+     *
+     * Example (DNA):
+     * @verbatim
      * A,T: false
      * A,A: true
      * A,N: true
      * A,Y: false
      * N,Y: true
      * N,N: true
+     * @endverbatim
      * 
      * @return True if the two characters are identical, or are compatible if at least one of them is a generic character.
      * @param alphabet The alphabet to use.
@@ -180,31 +188,30 @@ class AlphabetTools
      */
     static bool match(const Alphabet * alphabet, int i, int j)
     {
-//       vector<int> a = alphabet->getAlias(i);
-//       vector<int> b = alphabet->getAlias(j);
-//       vector<int> u = VectorTools::vectorIntersection(a, b);
-//       return u.size() > 0;
-      return 1;
+      vector<int> a = alphabet->getAlias(i);
+      vector<int> b = alphabet->getAlias(j);
+      vector<int> u = VectorTools::vectorIntersection(a, b);
+      return u.size() > 0;
     }
 
-	private:
-	  template<class Y>
-	  static bool alphabetInheritsFrom(const Alphabet * alphabet)
-		{
-			try
+  private:
+    template<class Y>
+    static bool alphabetInheritsFrom(const Alphabet * alphabet)
+    {
+      try
       {
         const Y* t = dynamic_cast<const Y*>(alphabet);
-				return t!= NULL; //Solves strange behavior in new gcc?
-			}
+        return t!= NULL; //Solves strange behavior in new gcc?
+      }
       catch(exception & e)
       {
-			  return false;
-			}
-		}
+        return false;
+      }
+    }
 
 };
 
 } //end of namespace bpp.
 
-#endif	//_ALPHABETTOOLS_H_
+#endif  //_ALPHABETTOOLS_H_
 
