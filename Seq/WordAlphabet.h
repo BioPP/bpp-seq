20a0b67c8e7b7733b426bc8b94f3234708eb06ae
gueguen <gueguen@80559d87-eeef-40de-8cae-50fbbb9f2701>
2010-02-17 12:44:28 +0000

    uncrustify

--- Seq/WordAlphabet.h
+++ Seq/WordAlphabet.h
@@ -5,37 +5,37 @@
 //
 
 /*
-Copyright or © or Copr. CNRS, (November 17, 2004)
+   Copyright or © or Copr. CNRS, (November 17, 2004)
 
-This software is a computer program whose purpose is to provide classes
-for sequences analysis.
+   This software is a computer program whose purpose is to provide classes
+   for sequences analysis.
 
-This software is governed by the CeCILL license under French law and
-abiding by the rules of distribution of free software. You can use,
-modify and/ or redistribute the software under the terms of the CeCILL
-license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info".
+   This software is governed by the CeCILL license under French law and
+   abiding by the rules of distribution of free software. You can use,
+   modify and/ or redistribute the software under the terms of the CeCILL
+   license as circulated by CEA, CNRS and INRIA at the following URL
+   "http://www.cecill.info".
 
-As a counterpart to the access to the source code and rights to copy,
-modify and redistribute granted by the license, users are provided
-only with a limited warranty and the software's author, the holder of
-the economic rights, and the successive licensors have only limited
-liability.
+   As a counterpart to the access to the source code and rights to copy,
+   modify and redistribute granted by the license, users are provided
+   only with a limited warranty and the software's author, the holder of
+   the economic rights, and the successive licensors have only limited
+   liability.
 
-In this respect, the user's attention is drawn to the risks associated
-with loading, using, modifying and/or developing or reproducing the
-software by the user in light of its specific status of free software,
-that may mean that it is complicated to manipulate, and that also
-therefore means that it is reserved for developers and experienced
-professionals having in-depth computer knowledge. Users are therefore
-encouraged to load and test the software's suitability as regards
-their requirements in conditions enabling the security of their
-systems and/or data to be ensured and, more generally, to use and
-operate it in the same conditions as regards security.
+   In this respect, the user's attention is drawn to the risks associated
+   with loading, using, modifying and/or developing or reproducing the
+   software by the user in light of its specific status of free software,
+   that may mean that it is complicated to manipulate, and that also
+   therefore means that it is reserved for developers and experienced
+   professionals having in-depth computer knowledge. Users are therefore
+   encouraged to load and test the software's suitability as regards
+   their requirements in conditions enabling the security of their
+   systems and/or data to be ensured and, more generally, to use and
+   operate it in the same conditions as regards security.
 
-The fact that you are presently reading this means that you have had
-knowledge of the CeCILL license and that you accept its terms.
-*/
+   The fact that you are presently reading this means that you have had
+   knowledge of the CeCILL license and that you accept its terms.
+ */
 
 #ifndef _WORDALPHABET_H_
 #define _WORDALPHABET_H_
@@ -50,29 +50,27 @@ knowledge of the CeCILL license and that you accept its terms.
 
 namespace bpp
 {
-
 /**
  * @brief The abstract base class for word alphabets.
- * 
+ *
  * These alphabets are compounds of several alphabets. The only
  * constraint on these alphabets is that their words have length one
  * (so it is not possible to make WordAlphabets from other
  * WordAlphabets). The construction is made from a vector of pointers
- * to AbstractAlphabets. 
+ * to AbstractAlphabets.
  *
  * The strings of the WordAlphabet are concatenations of the strings
  * of the Alphabets. They are made from the resolved letters of the
  * Alphabets.
  */
-class WordAlphabet:
+class WordAlphabet :
   public AbstractAlphabet
 {
 protected:
-
   std::vector<const Alphabet* > vAbsAlph_;
 
-public: // Constructor and destructor.
-		
+public:
+  // Constructor and destructor.
   /**
    * @brief Builds a new word alphabet from a vector of Alphabets.
    *
@@ -82,7 +80,7 @@ public: // Constructor and destructor.
    * @param vAlpha The vector of Alphabets to be used.
    */
   WordAlphabet(const std::vector<const Alphabet*>& vAlpha);
-  
+
   /**
    * @brief Builds a new word alphabet from a pointer to number of
    * Alphabets.
@@ -91,11 +89,10 @@ public: // Constructor and destructor.
    * @param num the length of the words.
    */
   WordAlphabet(const Alphabet* pAlpha, unsigned int num);
-  
+
   virtual ~WordAlphabet() {}
-  
+
 public:
-  
   /**
    * @name Methods redefined from Alphabet
    *
@@ -118,29 +115,29 @@ public:
     if (state.size() != vAbsAlph_.size())
       throw BadCharException(state, "WordAlphabet::charToInt", this);
     if (containsUnresolved(state))
-      return(getSize()); 
+      return getSize();
     if (containsGap(state))
       return -1;
-    else return AbstractAlphabet::charToInt(state);	
+    else return AbstractAlphabet::charToInt(state);
   }
 
   unsigned int getSize() const
   {
     return getNumberOfChars() - 2;
   }
-  
+
   /** @} */
 
   /**
-   * @brief Returns is the letters Alphabet in the word are the same
-   * type
-   * 
+   * @brief Returns True if the Alphabet of the letters in the word
+   * are the same type.
+   *
    */
   bool hasUniqueAlphabet() const;
 
-    /**
+  /**
    * @brief Returns the length of the word
-   * 
+   *
    */
   unsigned int getLength() const
   {
@@ -150,14 +147,13 @@ public:
 
   /**
    * @brief Returns the number of resolved states + one for unresolved
-   * 
+   *
    */
-
   unsigned int getNumberOfTypes() const
   {
     return getNumberOfChars() - 1;
   }
-  
+
   std::string getAlphabetType() const;
   int getUnknownCharacterCode() const
   {
@@ -173,34 +169,32 @@ public:
   std::string getGeneric(const std::vector<std::string>& states) const throw (BadCharException);
 
 private:
-  
   /**
    * @name Inner utilitary functions
    *
    * @{
    */
-  bool containsUnresolved(const std::string & state) const throw (BadCharException);
-  bool containsGap(const std::string & state) const throw (BadCharException);
+  bool containsUnresolved(const std::string& state) const throw (BadCharException);
+  bool containsGap(const std::string& state) const throw (BadCharException);
   void build_();
   /** @} */
 
 public:
-  
   /**
    * @name Word specific methods
    *
    * @{
    */
-  
+
   /**
    * @brief Get the pointer to the Alphabet  of the n-position.
-   * 
+   *
    * @param n The position in the word (starting at 0).
    * @return The pointer to the Alphabet of the n-position.
    */
   const Alphabet* getNAlphabet(unsigned int n) const
   {
-    if (n>=vAbsAlph_.size())
+    if (n >= vAbsAlph_.size())
       throw BadIntException(n, "WordAlphabet::getNPosition", this);
 
     return vAbsAlph_[n];
@@ -215,7 +209,7 @@ public:
    * @return The int code of the word.
    */
   virtual int getWord(const std::vector<int>& vint, unsigned int pos = 0) const throw (BadIntException);
-  
+
   /**
    * @brief Get the char code for a word given the char code of the
    * underlying positions.
@@ -226,62 +220,62 @@ public:
    * @return The string of the word.
    */
   virtual std::string getWord(const std::vector<std::string>& vpos, unsigned int pos = 0) const throw (BadIntException, BadCharException);
-  
+
   /**
    * @brief Get the int code of the n-position of a word given its int description.
-   * 
+   *
    * @param word The int description of the word.
    * @param n The position in the word (starting at 0).
    * @return The int description of the n-position of the word.
    */
-
   int getNPosition (int word, unsigned int n) const throw (BadIntException)
   {
-    if (n>=vAbsAlph_.size())
+    if (n >= vAbsAlph_.size())
       throw BadIntException(n, "WordAlphabet::getNPosition", this);
-  
+
     std::string s = intToChar(word);
     return vAbsAlph_[n]->charToInt(s.substr(n,1));
   }
 
   /**
    * @brief Get the int codes of each position of a word given its int description.
-   * 
+   *
    * @param word The int description of the word.
    * @return The int description of the positions of the codon.
    */
 
   std::vector<int> getPositions(int word) const throw (BadIntException)
   {
-    std::string s = intToChar(word);
-    std::vector<int> positions;
+   std::string s = intToChar(word);
+   std::vector<int> positions;
     for (unsigned int i = 0; i < s.size(); i++)
-      positions.push_back(vAbsAlph_[i]->charToInt(s.substr(i, 1)));
+    {
+   positions.push_back(vAbsAlph_[i]->charToInt(s.substr(i, 1)));
+    }
 
     return positions;
   }
   /**
    * @brief Get the char code of the n-position of a word given its char description.
-   * 
+   *
    * @param word The char description of the word.
    * @param n The position in the word (starting at 0).
    * @return The char description of the n-position of the word.
    */
-
   std::string getNPosition (const std::string& word, unsigned int n) const throw (BadCharException)
   {
-    if (n>vAbsAlph_.size())
+    if (n > vAbsAlph_.size())
       throw BadCharException("", "WordAlphabet::getNPosition", this);
-    //Test:
+    // Test:
     charToInt(word);
-  
+
     return "" + word.substr(n, 1);
   }
 
-  
+
   /**
    * @brief Get the char codes of each position of a word given its char description.
-   * 
+   *
    * @param word The char description of the word.
    * @return The char description of the three positions of the word.
    */
@@ -291,7 +285,9 @@ public:
     charToInt(word);
     std::vector<std::string> positions;
     for (unsigned int i = 0; i < word.size(); i++)
-      positions.push_back(word.substr(i, 1));
+    {
+   positions.push_back(word.substr(i, 1));
+    }
 
     return positions;
   }
@@ -305,7 +301,7 @@ public:
    * @throw AlphabetMismatchException If the sequence alphabet do not match the source alphabet.
    * @throw Exception                 Other kind of error, depending on the implementation.
    */
-  Sequence* translate(const Sequence& sequence, int pos=0) const throw (AlphabetMismatchException, Exception);
+  Sequence* translate(const Sequence& sequence, int pos = 0) const throw (AlphabetMismatchException, Exception);
 
   /**
    * @brief Translate a whole sequence from words alphabet to letters alphabet.
@@ -316,7 +312,7 @@ public:
    * @throw Exception                 Other kind of error, depending on the implementation.
    */
   Sequence* reverse(const Sequence& sequence) const throw (AlphabetMismatchException, Exception);
-  
+
   /** @} */
 
   /**
@@ -326,8 +322,7 @@ public:
   unsigned int getStateCodingSize() const { return vAbsAlph_.size(); }
   /** @} */
 };
+} // end of namespace bpp.
 
-} //end of namespace bpp.
-
-#endif	//_WORDALPHABET_H_
+#endif  // _WORDALPHABET_H_
 
