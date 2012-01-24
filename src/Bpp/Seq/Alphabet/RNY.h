07138d3e08d8c44e9c4207a238e89fb7742aa375
Julien Dutheil <jy.dutheil@gmail.com>
2012-01-24 09:11:09 +0100

    Cosmetic changes.

--- src/Bpp/Seq/Alphabet/RNY.h
+++ src/Bpp/Seq/Alphabet/RNY.h
@@ -5,7 +5,7 @@
 //
 
 /*
-   Copyright or © or Copr. CNRS, (November 17, 2004)
+   Copyright or © or Copr. Bio++ Development Team, (November 17, 2004)
 
    This software is a computer program whose purpose is to provide classes
    for sequences analysis.
@@ -47,7 +47,7 @@
 #include <string>
 
 /**
- * @brief This alphabet is used to deal triplet RNY + "---"
+ * @brief This alphabet is used to deal with triplet RNY + "---"
  * @author Laurent Guéguen
  */
 
