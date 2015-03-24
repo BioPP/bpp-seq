7e56beec3eaead71055ede67aa433b4a418ffe16
Laurent Gueguen <laurent.gueguen@univ-lyon1.fr>
2015-03-24 13:52:47 +0100

    site selection on possible positions

--- src/Bpp/Seq/App/SequenceApplicationTools.cpp
+++ src/Bpp/Seq/App/SequenceApplicationTools.cpp
@@ -424,10 +424,14 @@ VectorSiteContainer* SequenceApplicationTools::getSiteContainer(
         for (size_t i = 0; i < vSite1.size(); ++i)
         {
           int x = (vSite1[i] >= 0 ? vSite1[i] : static_cast<int>(nbSites) + vSite1[i]);
-          if (x >= 0)
-            vSite.push_back(static_cast<size_t>(x - 1));
-          else
-            throw Exception("SequenceApplicationTools::getSiteContainer(). Incorrect negative index: " + TextTools::toString(x));
+          if (x<=(int)nbSites)
+          {
+            if (x >= 0)
+              vSite.push_back(static_cast<size_t>(x - 1));
+            else
+              throw Exception("SequenceApplicationTools::getSiteContainer(). Incorrect negative index: " + TextTools::toString(x));
+          }
+          
         }
         selectedSites = dynamic_cast<VectorSiteContainer*>(SiteContainerTools::getSelectedSites(*sites, vSite));
         selectedSites->reindexSites();
