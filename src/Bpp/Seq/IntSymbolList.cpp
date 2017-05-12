602e99aadb8d2d1b7f8b0e0c0e5ddec5c9994b44
Laurent Gueguen <laurent.gueguen@univ-lyon1.fr>
2017-05-12 07:11:28 +0200

    newprobseq on work

---
+++ src/Bpp/Seq/IntSymbolList.cpp
@@ -0,0 +1,261 @@
+//
+// File: IntSymbolList.cpp
+// Created by: Julien Dutheil
+// Created on: Fri Apr 9 2005
+//
+
+/*
+  Copyright or © or Copr. Bio++ Development Team, (November 17, 2004)
+
+  This software is a computer program whose purpose is to provide classes
+  for sequences analysis.
+
+  This software is governed by the CeCILL  license under French law and
+  abiding by the rules of distribution of free software.  You can  use, 
+  modify and/ or redistribute the software under the terms of the CeCILL
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info". 
+
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited
+  liability. 
+
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or 
+  data to be ensured and,  more generally, to use and operate it in the 
+  same conditions as regards security. 
+
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#include "IntSymbolList.h"
+#include "StringSequenceTools.h"
+
+using namespace bpp;
+
+using namespace std;
+
+/****************************************************************************************/
+
+BasicIntSymbolList::BasicIntSymbolList(const std::vector<string>& list, const Alphabet* alpha) throw (BadCharException) :
+  SymbolList<int>(alpha)
+{
+  setContent(list);
+}
+
+BasicIntSymbolList::BasicIntSymbolList(const std::vector<int>& list, const Alphabet* alpha) throw (BadIntException) :
+  SymbolList<int>(alpha)
+{
+  setContent(list);
+}
+
+/****************************************************************************************/
+
+// BasicIntSymbolList::BasicIntSymbolList(const IntSymbolList& list):
+//   alphabet_(list.getAlphabet()), content_(list.size())
+// {
+//   for (size_t i = 0; i < list.size(); ++i)
+//     content_[i] = list[i];
+// }
+
+BasicIntSymbolList::BasicIntSymbolList(const BasicIntSymbolList& list):
+  SymbolList<int>(list) {}
+
+// BasicIntSymbolList& BasicIntSymbolList::operator=(const IntSymbolList& list)
+// {
+//   content_.resize(list.size());
+//   for (size_t i = 0; i < list.size(); ++i)
+//     content_[i] = list[i];
+//   alphabet_ = list.getAlphabet();
+//   return *this;
+// }
+
+BasicIntSymbolList& BasicIntSymbolList::operator=(const BasicIntSymbolList& list)
+{
+  SymbolList<int>::operator=(list);
+  return *this;
+}
+
+/****************************************************************************************/
+
+void BasicIntSymbolList::setContent(const vector<string>& list) throw (BadCharException)
+{
+  // Check list for incorrect characters
+  vector<int> coded(list.size());
+  for (size_t i = 0; i < list.size(); i++)
+    if(!alphabet_->isCharInAlphabet(list[i])) throw BadCharException(list[i], "BasicIntSymbolList::setContent", alphabet_);
+  
+  for (size_t i = 0; i < list.size(); i++) 
+    coded[i] = alphabet_->charToInt(list[i]);
+	
+  //BasicIntSymbolList is valid:
+  SymbolList<int>::setContent(coded);
+}
+
+/****************************************************************************************/
+
+void BasicIntSymbolList::setContent(const vector<int>& list)
+{
+  // Check list for incorrect characters
+  for (size_t i = 0; i < list.size(); i++)
+    if(!alphabet_->isIntInAlphabet(list[i]))
+      throw BadIntException(list[i], "BasicIntSymbolList::setContent", alphabet_);
+	
+  //Sequence is valid:
+  SymbolList<int>::setContent(coded);
+}
+
+/****************************************************************************************/
+
+string BasicIntSymbolList::toString() const
+{
+  return StringSequenceTools::decodeSequence(content_, alphabet_);
+};
+
+/****************************************************************************************/
+
+void BasicIntSymbolList::addElement(const string& c)
+{
+  content_.push_back(alphabet_->charToInt(c));
+}
+
+/****************************************************************************************/
+
+void BasicIntSymbolList::addElement(size_t pos, const string& c)
+{
+  if(pos >= content_.size()) throw IndexOutOfBoundsException("BasicIntSymbolList::addElement. Invalid position.", pos, 0, size() - 1);
+  content_.insert(content_.begin() + static_cast<ptrdiff_t>(pos), alphabet_->charToInt(c));
+}
+
+/****************************************************************************************/
+
+void BasicIntSymbolList::setElement(size_t pos, const string& c) 
+{
+  if(pos >= content_.size())
+    throw IndexOutOfBoundsException("BasicIntSymbolList::setElement. Invalid position.", pos, 0, size() - 1);
+  content_[pos] = alphabet_->charToInt(c);
+}
+
+/****************************************************************************************/
+
+string BasicIntSymbolList::getChar(size_t pos) const throw (IndexOutOfBoundsException)
+{
+  if(pos >= content_.size())
+    throw IndexOutOfBoundsException("BasicIntSymbolList::getChar. Invalid position.", pos, 0, size() - 1);
+  string c = "";
+  try
+  {
+    c = alphabet_->intToChar(content_[pos]);
+  }
+  catch(BadIntException bie)
+  {
+    //This should never happen!
+  }
+  return c;
+}
+
+/****************************************************************************************/
+
+EdIntSymbolList::EdIntSymbolList(const std::vector<string>& list, const Alphabet* alpha) throw (BadCharException) :
+  EdSymbolList<int>(alpha)
+{
+  setContent(list);
+}
+
+EdIntSymbolList::EdIntSymbolList(const std::vector<int>& list, const Alphabet* alpha) throw (BadIntException) :
+  EdSymbolList<int>(list, alpha)
+{
+}
+
+/****************************************************************************************/
+
+EdIntSymbolList::EdIntSymbolList(const EdIntSymbolList& list):
+  EdSymbolList<int>(list)
+{
+}
+
+EdIntSymbolList& EdIntSymbolList::operator=(const EdIntSymbolList& list)
+{
+  EdSymbolList<int>::operator=(list);
+  return *this;
+}
+
+/****************************************************************************************/
+
+void EdIntSymbolList::setContent(const vector<string>& list) throw (BadCharException)
+{
+  // Check list for incorrect characters
+  vector<int> coded(list.size());
+  for (size_t i = 0; i < list.size(); i++)
+    if (!alphabet_->isCharInAlphabet(list[i])) throw BadCharException(list[i], "EdIntSymbolList::setContent", alphabet_);
+	
+  for (size_t i = 0; i < list.size(); i++) 
+    coded[i] = alphabet_->charToInt(list[i]);
+
+  EdSymbolList<int>::setContent(coded);
+};
+
+/****************************************************************************************/
+
+void EdIntSymbolList::setContent(const vector<int>& list)
+{
+  // Check list for incorrect characters
+  for (size_t i = 0; i < list.size(); i++)
+    if(!alphabet_->isIntInAlphabet(list[i]))
+      throw BadIntException(list[i], "EdIntSymbolList::setContent", alphabet_);
+
+  EdSymbolList<int>::setContent(list);
+};
+
+/****************************************************************************************/
+
+string EdIntSymbolList::toString() const
+{
+  return StringSequenceTools::decodeSequence(content_, alphabet_);
+};
+
+/****************************************************************************************/
+
+void EdIntSymbolList::addElement(const string& c)
+{
+  EdSymbolList<int>::addElement(alphabet_->charToInt(c));
+}
+
+/****************************************************************************************/
+
+void EdIntSymbolList::addElement(size_t pos, const string& c)
+{
+  EdSymbolList<int>::addElement(pos, alphabet_->charToInt(c));
+}
+
+/****************************************************************************************/
+
+void EdIntSymbolList::setElement(size_t pos, const string& c) 
+{
+  EdSymbolList<int>::setElement(pos, alphabet_->charToInt(c));
+}
+
+/****************************************************************************************/
+
+string EdIntSymbolList::getChar(size_t pos) const throw (IndexOutOfBoundsException)
+{
+  if (pos >= content_.size())
+    throw IndexOutOfBoundsException("EdIntSymbolList::getChar. Invalid position.", pos, 0, size() - 1);
+  string c = "";
+  try {
+    c = alphabet_->intToChar(content_[pos]);
+  } catch(BadIntException bie) {
+    //This should never happen!
+  }
+  return c;
+}
+
---
+++ src/Bpp/Seq/IntSymbolList.h
@@ -0,0 +1,433 @@
+//
+// File: IntSymbolList.h
+// Created by: Julien Dutheil
+// Created on: Fri Apr 9 2005
+//
+
+/*
+Copyright or © or Copr. Bio++ Development Team, (November 17, 2004)
+
+This software is a computer program whose purpose is to provide classes
+for sequences analysis.
+
+This software is governed by the CeCILL  license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#ifndef _INTSYMBOLLIST_H_
+#define _INTSYMBOLLIST_H_
+
+#include "Alphabet/Alphabet.h"
+#include <Bpp/Clonable.h>
+#include "SymbolList.h"
+
+// From the STL:
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+namespace bpp
+{
+
+  /**
+   * @brief The specific IntSymbolList interface.
+   *
+   * @see Alphabet
+   */
+  class IntSymbolList :
+    virtual public SymbolList<int>
+  {
+  public:
+    IntSymbolList() :
+      SymbolList<int>(0)
+    {
+    }
+    
+    
+    // Class destructor
+    virtual ~IntSymbolList() {}
+
+  public:
+
+    /**
+     * @name Acting on the content of the list.
+     *
+     */
+
+    /**
+     * @brief Set the whole content of the list.
+     *
+     * @param list The new content of the list.
+     * @see The list constructor for information about the way lists are internaly stored.
+     */
+
+    virtual void setContent(const std::vector<std::string>& list) throw (BadCharException) = 0;
+
+    /**
+     * @name Edition methods.
+     *
+     * @{
+     */
+
+    /**
+     * @brief Add a character to the end of the list.
+     *
+     * @param c The character to add, given as a string.
+     */
+    virtual void addElement(const std::string& c) = 0;
+
+    /**
+     * @brief Add a character at a certain position in the list.
+     *
+     * @param pos The postion where to insert the element.
+     * @param c   The character to add, given as a string.
+     */
+    virtual void addElement(size_t pos, const std::string& c) = 0;
+    
+    /**
+     * @brief Set the element at position 'pos' to character 'c'.
+     *
+     * @param pos The position of the character to set.
+     * @param c   The value of the element, given as a string.
+     */
+    virtual void setElement(size_t pos, const std::string& c) = 0;
+    
+    /**
+     * @brief Get the element at position 'pos' as a character.
+     *
+     * @param pos The position of the character to retrieve.
+     */
+    virtual std::string getChar(size_t pos) const throw (IndexOutOfBoundsException) = 0;
+
+    /**
+     * @brief Get the element at position 'pos' as an int.
+     *
+     * @param pos The position of the character to retrieve.
+     */
+
+    virtual int getValue(size_t pos) const
+    {
+      return getElement(pos);
+    }
+
+    /** @} */
+
+  };
+
+
+  /**
+   * @brief A basic IntSymbolList object.
+   *
+   * This is a general purpose container, containing an ordered list of states(= letters).
+   * The states that allowed to be present in the list are defined by an alphabet object,
+   * which is passed to the list constructor by a pointer.
+   *
+   * For programming convenience, the states are stored as integers, but the translation toward
+   * and from a char description is easily performed with the Alphabet classes.
+   *
+   * @see Alphabet
+   */
+  
+  class BasicIntSymbolList: 
+    public virtual IntSymbolList
+  {
+  public:
+    BasicIntSymbolList(const Alphabet* alpha) : SymbolList<int>(alpha)
+    {
+    }
+    
+
+    /**
+     * @brief Build a new BasicIntSymbolList object with the specified alphabet.
+     * The content of the site is initialized from a vector of characters.
+     *
+     * @param list     The content of the site.
+     * @param alpha    The alphabet to use.
+     * @throw BadCharException If the content does not match the specified alphabet.
+     */
+    BasicIntSymbolList(const std::vector<std::string>& list, const Alphabet* alpha) throw (BadCharException);
+
+    /**
+     * @brief Build a new BasicIntSymbolList object with the specified alphabet.
+     * The content of the site is initialized from a vector of integers.
+     *
+     * @param list     The content of the site.
+     * @param alpha    The alphabet to use.
+     * @throw BadIntException If the content does not match the specified alphabet.
+     */
+    BasicIntSymbolList(const std::vector<int>& list, const Alphabet* alpha) throw (BadIntException);
+
+    /**
+     * @brief The generic copy constructor.
+     */
+//    BasicIntSymbolList(const IntSymbolList& list);
+
+    /**
+     * @brief The copy constructor.
+     */
+    BasicIntSymbolList(const BasicIntSymbolList& list);
+
+    /**
+     * @brief The generic assignment operator.
+     */
+    BasicIntSymbolList& operator=(const IntSymbolList& list);
+
+    /**
+     * @brief The assignment operator.
+     */
+    BasicIntSymbolList& operator=(const BasicIntSymbolList& list);
+
+    /**
+     * @name The Clonable interface
+     *
+     * @{
+     */
+    BasicIntSymbolList* clone() const { return new BasicIntSymbolList(* this); }
+    /** @} */
+
+    // Class destructor
+    virtual ~BasicIntSymbolList() {}
+    
+  public:
+
+    void setContent(const std::vector<int>& list);
+
+    void setContent(const std::vector<std::string>& list) throw (BadCharException);
+
+    std::string toString() const;
+
+    virtual void addElement(const std::string& c);
+
+    virtual void addElement(size_t pos, const std::string& c);
+
+    virtual void setElement(size_t pos, const std::string& c);
+
+    virtual std::string getChar(size_t pos) const throw (IndexOutOfBoundsException);
+
+  };
+
+  typedef CoreSymbolListListener<int> IntSymbolListListener;
+
+  typedef CoreSymbolListEditionEvent<int> IntSymbolListEditionEvent;
+
+  typedef CoreSymbolListInsertionEvent<int> IntSymbolListInsertionEvent;
+
+  typedef CoreSymbolListDeletionEvent<int> IntSymbolListDeletionEvent;
+
+  typedef CoreSymbolListSubstitutionEvent<int> IntSymbolListSubstitutionEvent;
+
+  /**
+   * @brief A event-driven IntSymbolList object.
+   *
+   * This is a general purpose container, containing an ordered list of states(= letters).
+   * The states that allowed to be present in the list are defined by an alphabet object,
+   * which is passed to the list constructor by a pointer.
+   *
+   * For programming convenience, the states are stored as integers, but the translation toward
+   * and from a char description is easily performed with the Alphabet classes.
+   *
+   * @see Alphabet
+   */
+
+  class EdIntSymbolList: 
+    virtual public EdSymbolList<int>
+  {
+  public: 
+    /**
+     * @brief Build a new void BasicIntSymbolList object with the specified alphabet.
+     *
+     * @param alpha The alphabet to use.
+     */
+    EdIntSymbolList(const Alphabet* alpha) : EdSymbolList<int>(alpha) 
+    {
+    }
+    
+
+    /**
+     * @brief Build a new BasicIntSymbolList object with the specified alphabet.
+     * The content of the site is initialized from a vector of characters.
+     *
+     * @param list     The content of the site.
+     * @param alpha    The alphabet to use.
+     * @throw BadCharException If the content does not match the specified alphabet.
+     */
+    EdIntSymbolList(const std::vector<std::string>& list, const Alphabet* alpha) throw (BadCharException);
+
+    /**
+     * @brief Build a new BasicIntSymbolList object with the specified alphabet.
+     * The content of the site is initialized from a vector of integers.
+     *
+     * @param list     The content of the site.
+     * @param alpha    The alphabet to use.
+     * @throw BadIntException If the content does not match the specified alphabet.
+     */
+    EdIntSymbolList(const std::vector<int>& list, const Alphabet* alpha) throw (BadIntException);
+
+    /**
+     * @brief The generic copy constructor.
+     */
+    EdIntSymbolList(const IntSymbolList& list);
+
+    /**
+     * @brief The copy constructor.
+     */
+    EdIntSymbolList(const EdIntSymbolList& list);
+
+    /**
+     * @brief The generic assignment operator.
+     */
+    EdIntSymbolList& operator=(const IntSymbolList& list);
+
+    /**
+     * @brief The assignment operator.
+     */
+    EdIntSymbolList& operator=(const EdIntSymbolList& list);
+
+    /**
+     * @name The Clonable interface
+     *
+     * @{
+     */
+    EdIntSymbolList* clone() const { return new EdIntSymbolList(* this); }
+    /** @} */
+
+    ~EdIntSymbolList() 
+    {
+    }
+    
+  public:
+
+    virtual void setContent(const std::vector<std::string>& list) throw (BadCharException);
+
+    virtual std::string toString() const;
+
+    virtual void addElement(const std::string& c);
+
+    virtual void addElement(size_t pos, const std::string& c);
+
+    virtual void setElement(size_t pos, const std::string& c);
+
+    virtual std::string getChar(size_t pos) const throw (IndexOutOfBoundsException);
+
+    // virtual const IntSymbolListListener& getListener(size_t i) const {
+    //   return dynamic_cast<const IntSymbolListListener&>(EdSymbolList<int>::getListener(i));
+    // }
+    
+    // virtual IntSymbolListListener& getListener(size_t i) {
+    //   return dynamic_cast<IntSymbolListListener&>(EdSymbolList<int>::getListener(i));
+    // }
+
+    void addIntSymbolListListener(IntSymbolListListener* listener)
+    {
+      addCoreSymbolListListener(listener);
+    }
+
+    // virtual void removeIntSymbolListListener(IntSymbolListListener* listener) {
+    //   if (listener->isRemovable())
+    //     listeners_.erase(remove(listeners_.begin(), listeners_.end(), listener), listeners_.end());
+    //   else
+    //     throw Exception("EdIntSymbolList::removeIntSymbolListListener. Listener is not removable.");
+    // } 
+ 
+  protected:
+    virtual void beforeSequenceChanged(const IntSymbolListEditionEvent& event) {};
+    virtual void afterSequenceChanged(const IntSymbolListEditionEvent& event) {};
+    virtual void beforeSequenceInserted(const IntSymbolListInsertionEvent& event) {};
+    virtual void afterSequenceInserted(const IntSymbolListInsertionEvent& event) {};
+    virtual void beforeSequenceDeleted(const IntSymbolListDeletionEvent& event) {};
+    virtual void afterSequenceDeleted(const IntSymbolListDeletionEvent& event) {};
+    virtual void beforeSequenceSubstituted(const IntSymbolListSubstitutionEvent& event) {};
+    virtual void afterSequenceSubstituted(const IntSymbolListSubstitutionEvent& event) {};
+
+  //   void fireBeforeSequenceChanged(const IntSymbolListEditionEvent& event) {
+  //     beforeSequenceChanged(event);
+  //     if (propagateEvents_)
+  //       for (size_t i = 0; i < listeners_.size(); ++i)
+  //         listeners_[i]->beforeSequenceChanged(event);
+  //   }
+
+  //   void fireAfterSequenceChanged(const IntSymbolListEditionEvent& event) {
+  //     afterSequenceChanged(event);
+  //     if (propagateEvents_)
+  //       for (size_t i = 0; i < listeners_.size(); ++i)
+  //         listeners_[i]->afterSequenceChanged(event);
+  //   }
+   
+  //   void fireBeforeSequenceInserted(const IntSymbolListInsertionEvent& event) {
+  //     beforeSequenceInserted(event);
+  //     if (propagateEvents_)
+  //       for (size_t i = 0; i < listeners_.size(); ++i)
+  //         listeners_[i]->beforeSequenceInserted(event);
+  //   }
+
+  //   void fireAfterSequenceInserted(const IntSymbolListInsertionEvent& event) {
+  //     afterSequenceInserted(event);
+  //     if (propagateEvents_)
+  //       for (size_t i = 0; i < listeners_.size(); ++i)
+  //         listeners_[i]->afterSequenceInserted(event);
+  //   }
+
+  //   void fireBeforeSequenceDeleted(const IntSymbolListDeletionEvent& event) {
+  //     beforeSequenceDeleted(event);
+  //     if (propagateEvents_)
+  //       for (size_t i = 0; i < listeners_.size(); ++i)
+  //         listeners_[i]->beforeSequenceDeleted(event);
+  //   }
+
+  //   void fireAfterSequenceDeleted(const IntSymbolListDeletionEvent& event) {
+  //     afterSequenceDeleted(event);
+  //     if (propagateEvents_)
+  //       for (size_t i = 0; i < listeners_.size(); ++i)
+  //         listeners_[i]->afterSequenceDeleted(event);
+  //   }
+
+  //   void fireBeforeSequenceSubstituted(const IntSymbolListSubstitutionEvent& event) {
+  //     beforeSequenceSubstituted(event);
+  //     if (propagateEvents_)
+  //       for (size_t i = 0; i < listeners_.size(); ++i)
+  //         listeners_[i]->beforeSequenceSubstituted(event);
+  //   }
+
+  //   void fireAfterSequenceSubstituted(const IntSymbolListSubstitutionEvent& event) {
+  //     afterSequenceSubstituted(event);
+  //     if (propagateEvents_)
+  //       for (size_t i = 0; i < listeners_.size(); ++i)
+  //         listeners_[i]->afterSequenceSubstituted(event);
+  //   }
+  //   /** @} */
+
+  // protected:
+  //   void propagateEvents(bool yn) { propagateEvents_ = yn; }
+  //   bool propagateEvents() const { return propagateEvents_; }
+
+  };
+
+
+} //end of namespace bpp.
+
+#endif // _INTSYMBOLLIST_H_
+
